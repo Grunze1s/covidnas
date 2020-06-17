fb678e468d215dadd9c2e94b446a5951b3d9c669
Sushil Khadka <punksusil.khadka@gmail.com>
2020-06-17 19:31:06 +0545

    Create user model
    created user model with username as phonenumber.
    added uuid in the user model.
    Closes:#2

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+from .managers import UserManager
+
+class User(AbstractUser):
+    phone_number = models.CharField(unique=True, max_length=20)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    username = None
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'
