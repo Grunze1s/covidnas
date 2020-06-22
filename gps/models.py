b9aecc5631761d89fe315a350d3eb40b1c1e9bd1
calcgen2 <raj.shrestha778@gmail.com>
2020-06-22 13:02:14 +0545

    Add test gps data. Add gps app for gps based tracing. Cluster the data using dbscan. Find radius and centres of clusters.

--- gps/models.py
+++ gps/models.py
@@ -1,14 +1,19 @@
+"""Define the models to store gps data"""
+
 from django.db import models
 from covidnas.models import User
 
 class Cluster(models.Model):
     longitude = models.FloatField(max_length=50)
     latitude = models.FloatField(max_length=50)
+    radius = models.FloatField(max_length=50)
+    label = models.IntegerField()
 
 class GPSData(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     latitude = models.FloatField(max_length=50)
     longitude = models.FloatField(max_length=50)
     cluster = models.ForeignKey(Cluster, on_delete=models.SET_NULL)
+    timestamp = models.DateTimeField()
 
 
